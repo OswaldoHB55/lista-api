9136f7408c657c2d0797def8684ae77155fbac4b
OswaldoHB55 <nestor.72607424@gmail.com>
2023-10-25 15:44:59 -0600

    www

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 import  express from "express"
 import {config} from "dotenv"
-config()
 
-import usuariosRoutes from "./routes/usuario.routes.js"
-
+import usuarioRoute from "./routes/usuario.routes.js"
+const app  = express(); //ejecutando la libreria.
+dotenv.config();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
-const app = express()
-
-app.use(express.json())
-
-app.use("/api/usuario", usuariosRoutes)
+app.use("/api", usuarioRoute)//
 
 app.listen(PORT, ()=> {
-    console.log("Servidor escuchando en el puerto http://localhost:" + PORT)
-})
+    console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+// config()
+
+// import usuariosRoutes from "./routes/usuario.routes.js"
+
+// const PORT = process.env.PORT || 3000;
+
+// const app = express()
+
+// app.use(express.json())
+
+// app.use("/api/usuario", usuariosRoutes)
+
+// app.listen(PORT, ()=> {
+//     console.log("Servidor escuchando en el puerto http://localhost:" + PORT)
+// })
--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -5,17 +5,74 @@ import path from 'path'
 const usuariosFile = path.join(process.cwd(), "data", "usuario.json")
 const router = Router() 
 
-router.get('/', (req, res)=> {
-    const usuarios = readFile()
-    res.json(usuarios);
+// Ruta para autenticar un usuario por nombre de usuario y contraseña
+router.post('/login', (req, res) => {
+    const { nameUser, password } = req.body;
+    const usuarios = ObtenerUsuarios(); // Debes definir la función ObtenerUsuarios() para cargar los datos de Usuario.json
+
+    // Buscar el usuario por nombre de usuario
+    const usuario = usuarios.find(user => user.nameUser === nameUser);
+
+    if (!usuario) {
+        return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    // Verificar la contraseña
+    if (usuario.password === password) {
+        return res.json({usuario });
+    } else {
+        return res.status(401).json({ message: 'Contraseña incorrecta' });
+    }
 });
 
+// Ruta para crear un nuevo usuario y validar el nombre de usuario
+router.post('/create', (req, res) => {
+    const { nameUser, password, Nombre, Apellido, Edad } = req.body;
+    const usuarios = ObtenerUsuarios(); // Debes definir la función ObtenerUsuarios() para cargar los datos de Usuario.json
 
-function readFile() {
-    const result = fs.readFileSync(usuariosFile, "utf-8")
-    const json = JSON.parse(result)
-    return json
+    // Verificar si el nombre de usuario ya está en uso
+    const usuarioExistente = usuarios.find(user => user.nameUser === nameUser);
+    if (usuarioExistente) {
+        return res.status(400).json({ message: 'El nombre de usuario ya está en uso' });
+    }
+
+    // Si el nombre de usuario no está en uso, crea un nuevo usuario
+    const nuevoUsuario = {
+        nameUser,
+        password,
+        Nombre,
+        Apellido,
+        Edad
+    };
+
+    // Agregar el nuevo usuario a la lista de usuarios
+    usuarios.push(nuevoUsuario);
+
+    // Guardar la lista actualizada de usuarios en el archivo JSON
+    fs.writeFileSync(usuariosFile, JSON.stringify(usuarios, null, 2));
+
+    return res.json({ message: 'Usuario creado exitosamente', usuario: nuevoUsuario });
+});
+
+// Función para cargar los datos de usuarios desde Usuario.json
+function ObtenerUsuarios() {
+    const usuariosFile = path.join(process.cwd(), 'data', 'Usuario.json');
+    const contenido = fs.readFileSync(usuariosFile, 'UTF-8');
+    return JSON.parse(contenido);
 }
 
-
 export default router
+
+// router.get('/', (req, res)=> {
+//     const usuarios = readFile()
+//     res.json(usuarios);
+// });
+
+
+// function readFile() {
+//     const result = fs.readFileSync(usuariosFile, "utf-8")
+//     const json = JSON.parse(result)
+//     return json
+// }
+
+
